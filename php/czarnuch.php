ce85b9a653ba25758c9955b99a8a87b35bb3cfc7
ZareMate <najama.mg@gmail.com>
2021-12-20 22:10:05 +0100

    zare

--- php/czarnuch.php
+++ php/czarnuch.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <link rel="icon" href="https://gdurl.com/Mwhp">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../style2.css">
     <title>czarnuch</title>
     <meta property="og:title" content="ZAKUP DOMENĘ CZARNUCH.PL" />
     <meta property="og:description" content="Mój ojciec Makumba być królem wioski
@@ -18,6 +19,7 @@
     I nikt tu nie lubić czarny człowiek" />
     <meta property="og:image" content="https://cdn.discordapp.com/attachments/833020152495865886/878570758568288256/giorgio.gif" />
 </head>
+
 <body>
     <div>
         elo elo wynajmę czarnuch.tech - kontakt discord <br>
@@ -28,8 +30,14 @@
     <video autoplay muted loop id="video1">
         <source src="https://cdn.glitch.me/b79dbdfc-b511-4e64-b306-9425899fc858%2Fnaturaltanczy.mp4?v=1636024243754" type="video/mp4">
         Your browser does not support HTML5 video.
-        </video>
-        <script>document.addEventListener("click",function(){var video=document.getElementById("video1");video.muted=false;});</script>
+    </video>
+    <script>
+        document.addEventListener("click", function() {
+            var video = document.getElementById("video1");
+            video.muted = false;
+        });
+    </script>
 
 </body>
+
 </html>
