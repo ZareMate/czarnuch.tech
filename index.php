6575d754f3deb24cd1cedee2a6ca694991ee1561
ZareMate <najama.mg@gmail.com>
2021-12-20 08:08:18 +0100

    zare

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
-<?php
-readfile('1.html');
-?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="utf-8" />
+  <title>Czarnuch - strona główna</title>
+  <link href="/css/dark-theme.css" rel="stylesheet" type="text/css" id="theme-link" />
+  <link href="/css/style.css" rel="stylesheet" type="text/css" />
+  <link rel="shortcut icon" type="image/x-icon" href="https://cdn.glitch.me/b79dbdfc-b511-4e64-b306-9425899fc858%2Fsearch.png?1633892612965" />
+  <meta name="description" content="Czarnuch i tyle ale możesz sobie obczaić">
+  <meta name="keywords" content="czarnuch, murzyn, tadzik">
+  <meta name="robots" content="index, czarnuch, murzyn, archives" />
+  <meta name="author" content="ZareMate">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="canonical" href="https://www.czarnuch.tech/" />
+</head>
+<body>
+  <div class="header"><br>
+    <div id="btn-div"><button class="btn-toggle" onclick="change()">Toggle nigger mode</button><br></div>
+    <h1>Czarnuch.tech</h1>
+    <div id="searchWrapper">
+      <input type="text" name="searchBar" id="searchBar" placeholder="search for a site" />
+    </div>
+  </div>
+  </div>
+
+  <div class="sitess">
+    <ul id="sites"></ul>
+  </div>
+  </br>
+  </br>
+  </br>
+  </br>
+  <div id="footer">
+    <p>JT team</p>
+  </div>
+  <script src="/js/search.js"></script>
+  <script src="/js/theme.js"></script>
+</body>
+</html>
