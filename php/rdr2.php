1ae4dbcd0bb2709db6b2c12b779653c28f2829e8
ZareMate <najama.mg@gmail.com>
2022-01-07 00:57:46 +0100

    zare

---
+++ php/rdr2.php
@@ -0,0 +1,232 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Legendary animals in Red Dead Online</title>
+    <link href="/css/rdr2.css" rel="stylesheet" type="text/css" id="theme-link" />
+    <link rel="shortcut icon" type="image/x-icon" href="https://cdn.glitch.me/b79dbdfc-b511-4e64-b306-9425899fc858%2Fsearch.png?1633892612965" />
+    <meta name="description" content="Czarnuch i tyle ale możesz sobie obczaić">
+    <meta name="keywords" content="rdr2, rdr online, red dead redemption 2, red dead redemption online">
+    <meta name="author" content="ZareMate">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="https://www.czarnuch.tech/" />
+</head>
+
+<body>
+    <div class="box">
+        <h3>All Red Dead Online legendary animals locations</h3>
+        <table class="darkTable">
+            <thead>
+                <tr>
+                    <th class="firstcol ">Legendary Animal</th>
+                    <th>Location</th>
+                    <th>Conditions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="firstcol ">Golden Spirit Bear</td>
+                    <td>Big Valley</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Night Beaver</td>
+                    <td>Roanoke Ridge</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Payta Bison</td>
+                    <td>Little Creek</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Icahai Boar</td>
+                    <td>East New Austin</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Sapa Cougar</td>
+                    <td>Northeast New Austin</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Milk Coyote</td>
+                    <td>Blackwater</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Inahme Elk</td>
+                    <td>Spider Gorge</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Cross Fox</td>
+                    <td>Bayou Nwa</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Banded Gator</td>
+                    <td>Saint Denis</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Ruddy Moose</td>
+                    <td>Tall Trees</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Rutile Horn Ram</td>
+                    <td>Rio Bravo and Cholla Springs</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Moonstone Wolf</td>
+                    <td>North of Cumberland Forest</td>
+                    <td>Harriet Mission</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Owiza Bear</td>
+                    <td>Dakota River</td>
+                    <td>Nighttime in rain</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Ridgeback Spirit Bear</td>
+                    <td>Little Creek River</td>
+                    <td>Daytime</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Moon Beaver</td>
+                    <td>Along the Kamassa River and Elysian Pool</td>
+                    <td>Dawn or dusk in rain</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Zizi Beaver</td>
+                    <td>Lake Owanjila</td>
+                    <td>Dawn or dusk in any weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Tatanka Bison</td>
+                    <td>East of Heartland Oil Fields</td>
+                    <td>Daytime in rain or drizzle</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Winyan Bison</td>
+                    <td>North of Lake Isabella</td>
+                    <td>Nighttime in clear weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Cogi Boar</td>
+                    <td>Bluewater Marsh</td>
+                    <td>Dawn in dry weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Wakpa Boar</td>
+                    <td>Stillwater Marsh</td>
+                    <td>Daytime in rain</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Mud Runner Buck</td>
+                    <td>Flat Iron Lake</td>
+                    <td>Daytime in dry weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Snow Buck</td>
+                    <td>Aurora Basin</td>
+                    <td>Dawn in dry weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Iguga Cougar</td>
+                    <td>Southeast Great Plains</td>
+                    <td>Dusk during storms</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Maza Cougar</td>
+                    <td>Sea of Coronado</td>
+                    <td>Dawn with clear weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Red Streak Coyote</td>
+                    <td>Pike's Basin</td>
+                    <td>Day and evening in any weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Midnight Paw Coyote</td>
+                    <td>Southeast of Strawberry</td>
+                    <td>Dawn and daytime in dry weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Katata Elk</td>
+                    <td>Cumberland Forest</td>
+                    <td>Daytime with heavy fog</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Ozula Elk</td>
+                    <td>Cholla Springs</td>
+                    <td>Dusk and nighttime in fog</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Marble Fox</td>
+                    <td>Colter</td>
+                    <td>Dawn or dusk in clear weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Ota Fox</td>
+                    <td>West of Rhodes</td>
+                    <td>Dawn or dusk in dry weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Sun Gator</td>
+                    <td>Near Lakay and Lagras</td>
+                    <td>Morning with heavy fog</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Teca Gator</td>
+                    <td>Lannahechee River south of Caliga Hall</td>
+                    <td>Nighttime during storms</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Knight Moose</td>
+                    <td>Northern Kamassa River</td>
+                    <td>Daytime</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Snowflake Moose</td>
+                    <td>Barrow Lagoon</td>
+                    <td>Nighttime in rain</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Nightwalker Panther</td>
+                    <td>Southwest of Bolger Glade</td>
+                    <td>Dusk in fog or mist</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Ghost Panther</td>
+                    <td>Merkins Waller and Macomb's End</td>
+                    <td>Nighttime in wet weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Chalk Horn Ram</td>
+                    <td>East of Calumet Ravine</td>
+                    <td>Day and dusk in clear weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Gabbro Horn Ram</td>
+                    <td>Rio Bravo</td>
+                    <td>Dawn and day in dry weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Emerald Wolf</td>
+                    <td>O'Creagh's Run</td>
+                    <td>Nighttime any weather</td>
+                </tr>
+                <tr>
+                    <td class="firstcol ">Onyx Wolf</td>
+                    <td>Between Wapiti and Cotorra Springs</td>
+                    <td>Nighttime dry weather</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
