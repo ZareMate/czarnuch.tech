cd3ea422ebbd71c09a9f07eed0d308e2235f6e99
ZareMate <zaremate@outlook.com>
2022-03-09 17:05:08 +0100

    .

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
+<?php
+$ip_address = $_SERVER["REMOTE_ADDR"]; // user ip adderss
+$filename = "ips.txt";
+$contents = file_get_contents($filename);
+$my_arr = json_decode($contents, true);
+if (in_array($ip_address, $my_arr)) {
+} else {
+    $my_arr[] = $ip_address;
+    file_put_contents($filename,  json_encode($my_arr));
+}
+?>
 <!DOCTYPE html>
 <html lang="pl">
 
 <head>
-    <?php
-    $ip_address = $_SERVER["REMOTE_ADDR"]; // user ip adderss
-    $filename = "ips.txt";
-    $contents = file_get_contents($filename);
-    $my_arr = json_decode($contents, true);
-    if (in_array($ip_address, $my_arr)) {
-    } else {
-        $my_arr[] = $ip_address;
-        file_put_contents($filename,  json_encode($my_arr));
-    }
-    ?>
     <meta charset="utf-8" />
     <title>Czarnuch - strona główna</title>
     <link href="/css/dark-theme.css" rel="stylesheet" type="text/css" id="theme-link" />
--- js/search.js
+++ js/search.js
@@ -25,7 +25,7 @@ const displayCharacters = (site) => {
   const htmlString = site
     .map((character) => {
       return `
-            <div class="characters, character"><div class="character">
+            <div class="character"><div class="character">
                 <h2><a class="btn-web" href="${character.a}">${character.name} <img src="${character.image}" width="25" height="25""></img></br><p class="desc">${character.desc}</p></a></h2>
             </div></div>  
         `;
