e8b48553c0a0f128a2aabf527ff791707a939847
ZareMate <zaremate@outlook.com>
2022-03-09 17:07:23 +0100

    .

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $ip_address = $_SERVER["REMOTE_ADDR"]; // user ip adderss
 $filename = "ips.txt";
 $contents = file_get_contents($filename);
 $my_arr = json_decode($contents, true);
-$agggg = "nigger";
+$aggg = "niggers";
 if (in_array($ip_address, $my_arr)) {
 } else {
     $my_arr[] = $ip_address;
