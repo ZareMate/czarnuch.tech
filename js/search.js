7dff98b6700cd8f5387db5b2705ccc201c59cac0
ZareMate <zaremate@outlook.com>
2022-03-10 19:09:19 +0100

    .

---
+++ js/search.js
@@ -0,0 +1,37 @@
+const sites = document.getElementById("sites");
+const searchBar = document.getElementById("searchBar");
+let site = [];
+
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredSites = site.filter((site) => {
+    return site.name.toLowerCase().includes(searchString);
+  });
+  displayCharacters(filteredSites);
+});
+
+const loadCharacters = async () => {
+  try {
+    const res = await fetch("sites.lib");
+    site = await res.json();
+    displayCharacters(site);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const displayCharacters = (site) => {
+  const htmlString = site
+    .map((character) => {
+      return `
+            <div class="characters"><li class="character">
+                <h2><a href="${character.a}">${character.name} <img src="${character.image}" width="25" height="25""></img></br><p class="desc">${character.desc}</p></a></h2>
+            </li></div>  
+        `;
+    })
+    .join("");
+  sites.innerHTML = htmlString;
+};
+
+loadCharacters();
